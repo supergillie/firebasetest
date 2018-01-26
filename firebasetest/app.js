9cd7fc42315beff236cc76fc0e8f015e44caf822
Peter Krooks <MNML@rsv.se>
2018-01-26 11:14:21 +0100

    funkar nu

---
+++ firebasetest/app.js
@@ -0,0 +1,26 @@
+(function(){
+
+	//Initialize firebase
+	const config = {
+
+				    apiKey: "AIzaSyCrOdVb-jhLKh37xMIz0ctKmx-hoqLx3Hk",
+				    authDomain: "pekr-3dad2.firebaseapp.com",
+				    databaseURL: "https://pekr-3dad2.firebaseio.com",
+				    storageBucket: "pekr-3dad2.appspot.com",
+	};
+	firebase.initializeApp(config);
+
+	//Get elements
+	const preObject = document.getElementById('object');
+
+	//Create references
+	const dbRefObject = firebase.database().ref().child('object');
+
+	//sync object changes
+//	dbRefObject.on('value', snap => console.log(snap.val()));
+	dbRefObject.on('value', snap => { preObject.innerText = JSON.stringify(snap.val(), null, 3)
+	});
+
+
+
+}());
